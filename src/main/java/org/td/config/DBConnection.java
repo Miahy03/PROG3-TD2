36e6b9c81f17507997d95e9ffd67c1d31321926b
Miahy03 <hei.miahy.2@gmail.com>
2026-01-07 09:01:46 +0100

    Correction du mot de pass dans DBConnection

--- src/main/java/org/td/config/DBConnection.java
+++ src/main/java/org/td/config/DBConnection.java
@@ -9,7 +9,7 @@ public class DBConnection {
 
     private static final String URL = "jdbc:postgresql://localhost:5432/restaurant_db";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "postgres";
+    private static final String PASSWORD = "miahy";
 
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
